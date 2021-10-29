22f0d42fb68df5eefb09411cc6123486a82a69f7
Patrick Bustos <saintpat97@gmail.com>
2021-10-28 22:23:07 -0700

    Added basic dynamodb updates to lambda

--- sales_tax/receipt_sales_tax.py
+++ sales_tax/receipt_sales_tax.py
@@ -2,6 +2,7 @@ import re
 import json
 import pprint
 import boto3
+from decimal import *
 from line_has_number import line_has_number
 
 # Is used with a get function that includes query parameters
@@ -19,6 +20,7 @@ def lambda_handler(event, context):
     bucket = event['queryStringParameters']['bucket']
     file = event['queryStringParameters']['file']
     user = event['queryStringParameters']['userId']
+    category = 'sales tax'
     filePath = str(user) + '/' + str(file)
     
     rekognition = boto3.client('rekognition', 'us-west-2')
@@ -80,20 +82,41 @@ def lambda_handler(event, context):
     if storeName == [] or storeName == None or len(storeName) == 0:
         storeName = ''
     if total == None:
-        total = ''
+        total = 0
     if tax == None:
-        tax = ''
+        tax = 0
     resDict = {
         'storeName': storeName,
         'total': total,
-        'taxAmount': tax
+        'taxAmount': tax,
+        'category': category
     }
     res = {
         'userId': user,
         'filePath': filePath,
         'receiptInfo': resDict
     }
+
+    dynamoUpdate(res)
     return {
         'statusCode': 200,
         'body': json.dumps(res)
     }
+
+def dynamoUpdate(res):
+    dynamodb = boto3.resource('dynamodb')
+    receiptTable = dynamodb.Table('itemize-receiptdb')
+    userTable = dynamodb.Table('itemize-userdb')
+    receiptTable.update_item(
+        Key={
+            'filePath': res['filePath']
+        },
+        UpdateExpression="SET userId=:userId, taxAmount=:tax, totalAmount=:totalAmount, category=:category",
+        ExpressionAttributeValues={
+                ':tax': Decimal(str(res['receiptInfo']['taxAmount'])),
+                ':totalAmount': Decimal(str(res['receiptInfo']['total'])),
+                ':userId': res['userId'],
+                ':category': res['receiptInfo']['category']
+        }
+        )
+    return
